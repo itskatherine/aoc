4dde489c22e66de5a371e0caf344cbe7f36f827c
itskatherine <hurstkatherine@hotmail.co.uk>
2022-12-09 15:07:44 +0000

    Finished day4

--- day4/part2.js
+++ day4/part2.js
@@ -1,17 +1,16 @@
 const data = require("./data");
 
-let overlap = 0;
+let overlaps = 0;
 const createTaskArray = (start, finish) => {
   const allTasks = [];
   const first = start;
-  for (let i = 1; i <= finish - start; i++) {
+  for (let i = 0; i <= finish - start; i++) {
     allTasks.push(first + i);
   }
   return allTasks;
 };
 const pairs = data.split("\n");
-let overlaps = 0;
-for (let i = 0; i < 1; i++) {
+for (let i = 0; i < pairs.length; i++) {
   const elf1Tasks = pairs[i]
     .split(",")[0]
     .split("-")
@@ -28,9 +27,10 @@ for (let i = 0; i < 1; i++) {
   console.log(expandedElf2Tasks, "elf2");
   for (let i = 0; i < expandedElf1Tasks.length; i++) {
     if (expandedElf2Tasks.includes(expandedElf1Tasks[i])) {
-      overlap++;
+      console.log("There's overlap");
+      overlaps++;
       break;
     }
   }
 }
-console.log(overlap);
+console.log(overlaps);
